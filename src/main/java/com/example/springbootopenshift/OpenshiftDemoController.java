5fed97ac03432a752b7b562def63d943f2eaf5cc
suparnabose30 <43331187+suparnabose30@users.noreply.github.com>
2019-06-13 15:18:06 +0530

    Add files via upload

--- src/main/java/com/example/springbootopenshift/OpenshiftDemoController.java
+++ src/main/java/com/example/springbootopenshift/OpenshiftDemoController.java
@@ -2,6 +2,7 @@ package com.example.springbootopenshift;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +13,10 @@ public class OpenshiftDemoController {
         @GetMapping("/test")
         public String printMessage(){
             return "Spring boot openshift deployment";
-    }
+        }
+
+        @GetMapping("/{user}")
+        public String printMessage(@PathVariable String user){
+            return "Hi "+user+" your spring boot openshift deployment completed";
+        }
 }
